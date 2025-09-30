5e603aa847eaf3c415333946ffd8745225afdfaa
hinata1108 <hinata1108.3420@yahoo.com>
2025-09-30 11:29:30 +0900

    first commit

---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import './main.css';
+
+export default function Header() {
+  return (
+<header className="header">
+    <p>About Me</p>
+    <nav >
+        <ul className="nav">
+            <li><a href="/Introdection">自己紹介</a></li>
+            <li><a href="/Result">スキルセット</a></li>
+            <li><a href="/Hobby">趣味</a></li>
+            <li><a href="/Activity">連絡先</a></li>
+        </ul>
+    </nav>
+</header>
+  )}
---
+++ src/Introduction.tsx
@@ -0,0 +1,19 @@
+import ReactLogo from './assets/react.svg';
+import './main.css';
+
+export default function Introduction() {
+    return (
+    <div className="introduction">
+            <p>自己紹介</p>
+      <div className="avatar_text">
+            <img src={ReactLogo} alt="hinata's avatar" width="270" height="300" />
+        <div className="text">
+            <p>愛知県名古屋市にある名城大学農学部2年生です。</p>
+<p>大学に入ってからプログラミングの勉強を始めました。</p>
+<p>現在はフロントエンドとデータサイエンスを勉強しています。</p>
+<p>またバイオインフォマティクス分野に興味があり、</p>
+<p>12月に検定に挑戦する予定です。</p>
+        </div>
+      </div>
+    </div>
+    )}
